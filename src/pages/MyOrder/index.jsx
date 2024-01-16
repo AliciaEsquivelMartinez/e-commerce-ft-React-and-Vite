4c487347488632e4059d24a099f522f4d059e423
Alicia Esquivel Martinez <aliiciaaesquiivel@gmail.com>
2024-01-16 15:15:09 -0600

    Nuevo flujo: al dar clic en checkout se nos abria la pagina de MylastOrder ahora aqui se tiene la posibilidad de regresar a la pagina de MyOrders para visualizar todas las orders (esto ultimo aun no esta funcionando)

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { ShoppiCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 import OrderCard from '../../Components/OrderCard'
@@ -8,20 +9,27 @@ function MyOrder() {
 
     return (
         <Layout>
-        MyOrder
-        <div className='flex flex-col w-80'>
-            {
-            context.order?.slice(-1)[0].products.map(product => (
-                <OrderCard
-                key={product.id}
-                id={product.id}
-                title={product.title}
-                imageUrl={product.images}
-                price={product.price}
-                />
-            ))
-            }
-        </div>
+            <div className='flex items-center justify-center relative w-80 mb-6'>
+                <Link to='/MyOrders' className='absolute left-0'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-black cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </Link>
+                <h1>My Order</h1>
+            </div>
+            <div className='flex flex-col w-80'>
+                {
+                context.order?.slice(-1)[0].products.map(product => (
+                    <OrderCard
+                    key={product.id}
+                    id={product.id}
+                    title={product.title}
+                    imageUrl={product.images}
+                    price={product.price}
+                    />
+                ))
+                }
+            </div>
         </Layout>
     )
 }
