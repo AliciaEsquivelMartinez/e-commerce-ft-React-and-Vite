05d553ce67bb7ae410b56322fd86f68966c835aa
Alicia Esquivel Martinez <aliiciaaesquiivel@gmail.com>
2024-01-16 15:34:45 -0600

    Estilos para las cards de MyOrders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -2,13 +2,20 @@ const OrdersCard = props => {
     const { totalPrice, totalProducts } = props
 
     return (
-        <div className="flex justify-between items-center mb-3 border border-black">
-        <p>
-            <span>15.01.24</span>
-            <span>{totalProducts}</span>
-            <span>{totalPrice}</span>
-        </p>
+        <div className="flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80">
+            <div className='flex justify-between w-full'>
+            <p className='flex flex-col'>
+                <span className='font-light'>01.02.23</span>
+                <span className='font-light'>{totalProducts} articles</span>
+            </p>
+            <p className='flex items-center gap-2'>
+                <span className='font-medium text-2xl'>${totalPrice}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </p>
         </div>
+    </div>
     )
 }
 
