770ff1e4d1c351a575a8f1c3864bc2006f71c08a
Alicia Esquivel Martinez <aliiciaaesquiivel@gmail.com>
2024-01-16 13:11:39 -0600

    Agregando productos al aside de myOrder, ya se puede visualizar los productos que se estan añadiendo

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,21 +1,34 @@
 import { useContext } from 'react'
 import { ShoppiCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppiCartContext)
+    console.log('CART: ', context.cartProducts)
 
     return (
-        <aside
-        className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-        <div className='flex justify-between items-center p-6'>
-            <h2 className='font-medium text-xl'>My Order</h2>
-            <div className='h-6 w-6 text-black cursor-pointer' onClick={() => context.closeCheckoutSideMenu()}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
+        <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>My Order</h2>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </div>
             </div>
-        </div>
+            <div className='px-6'>
+                {
+                context.cartProducts.map(product => (
+                    <OrderCard
+                    key={product.id}
+                    title={product.title}
+                    imageUrl={product.images}
+                    price={product.price}
+                    />
+                ))
+                }
+            </div>
         </aside>
     )
 }
