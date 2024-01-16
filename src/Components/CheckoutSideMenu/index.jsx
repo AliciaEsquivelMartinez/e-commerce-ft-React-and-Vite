8b13f9565ee9cff0486a8f9d8015462794a24f48
Alicia Esquivel Martinez <aliiciaaesquiivel@gmail.com>
2024-01-16 13:42:24 -0600

    Evitando añadir duplicacion de productos en el carrito, ademas, al agregar uno el icono de plus (+) cambia a check(palomita) para indicar que ya fue colocado en la bolsa de compra

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -5,26 +5,25 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppiCartContext)
-    console.log('CART: ', context.cartProducts)
 
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>My Order</h2>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <div onClick={() => context.closeCheckoutSideMenu()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-black cursor-pointer">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
                 </div>
             </div>
-            <div className='px-6'>
+            <div className='px-6 overflow-y-scroll'>
                 {
                 context.cartProducts.map(product => (
                     <OrderCard
-                    key={product.id}
-                    title={product.title}
-                    imageUrl={product.images}
-                    price={product.price}
+                        key={product.id}
+                        title={product.title}
+                        imageUrl={product.images}
+                        price={product.price}
                     />
                 ))
                 }
