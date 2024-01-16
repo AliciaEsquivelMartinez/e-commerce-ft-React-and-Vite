2e5a2c998de01e76298096fb8d4bec8b7f0be7a6
Alicia Esquivel Martinez <aliiciaaesquiivel@gmail.com>
2024-01-16 12:43:00 -0600

    Side menu del carrito de compras: al dar añadir un producto abre un aside al lado derecho e donde se mostrara posteriormente el articulo

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -9,9 +9,12 @@ const Card = (data) => {
         context.setProductToShow(productDetail)
     }
 
-    const addProductsToCart = (productData) => {
+    const addProductsToCart = (event, productData) => {
+        event.stopPropagation()
         context.setCount(context.count + 1)
         context.setCartProducts([...context.cartProducts, productData])
+        context.openCheckoutSideMenu()
+        context.closeProductDetail()
         console.log('CART: ', context.cartProducts)
     }
 
@@ -21,7 +24,7 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full h-4/5'>
             <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
             <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
-            <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => addProductsToCart(data.data)}>
+            <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={(event) => addProductsToCart(event, data.data)}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,32 +5,40 @@ export const ShoppiCartContext = createContext()
 
 //componente
 export const ShoppiCartProvider = ({children}) => {
-    //shopping card count 
+    //shoppi card count 
     const [count, setCount] = useState(0)
 
-    //open - close card
+    //open - close product detail card
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
 
+    // Checkout Side Menu · Open/Close
+    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
     //product detail - show product
     const [productToShow, setProductToShow] = useState({})
 
-    // Shopping Cart · Add products to cart
+    // Shoppi Cart · Add products to cart
     const [cartProducts, setCartProducts] = useState([])
 
     return (
         //proveedor
         <ShoppiCartContext.Provider value={{
             count,
-            setCount, 
+            setCount,
             openProductDetail,
-            closeProductDetail, 
-            isProductDetailOpen, 
+            closeProductDetail,
+            isProductDetailOpen,
             productToShow,
             setProductToShow,
             cartProducts,
-            setCartProducts
+            setCartProducts,
+            isCheckoutSideMenuOpen,
+            openCheckoutSideMenu,
+            closeCheckoutSideMenu
         }}>
             {children}
         </ShoppiCartContext.Provider>
